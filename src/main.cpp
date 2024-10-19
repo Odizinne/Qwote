98dc10c0450e14817a9e085154fc6642f5c6872d
Odizinne <flora.aubry@proton.me>
2024-10-19 10:29:27 +0200

    prevent multiple instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
-#include <QApplication>
 #include <qwote.h>
+#include <QApplication>
+#include <QSharedMemory>
 
 int main(int argc, char *argv[]) {
+    QSharedMemory sharedMemory("QwoteID");
+
+    if (sharedMemory.attach()) {
+        return 0;
+    }
+
+    if (!sharedMemory.create(1)) {
+        return 1;
+    }
+
     QApplication app(argc, argv);
     app.setStyle("fusion");
     app.setQuitOnLastWindowClosed(false);
-    Qwote w;
+    Qwote qwote;
     return app.exec();
 }
