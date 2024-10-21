4826a4390505d872a99142d4f74135c26b28a443
Odizinne <flora.aubry@proton.me>
2024-10-21 09:20:24 +0200

    QwoteServer in its own class

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,29 @@
-#include <qwote.h>
 #include <QApplication>
 #include <QSharedMemory>
-#include <QDebug>
-#include "QwoteServer.h"
+#include "qwoteserver.h"
+
+const QString SERVER_NAME = "QwoteServer";
 
 int main(int argc, char *argv[]) {
-    qDebug() << "Starting application";
     QSharedMemory sharedMemory("QwoteID");
 
-    // Check if another instance is running
     if (sharedMemory.attach()) {
-        qDebug() << "Attempting to connect to existing instance";
         QLocalSocket socket;
-        socket.connectToServer("QwoteServer");
+        socket.connectToServer(SERVER_NAME);
 
-        // Check if the connection was successful
-        if (socket.waitForConnected(3000)) { // Wait for 3 seconds for connection
+        if (socket.waitForConnected()) {
             QByteArray block;
             QDataStream out(&block, QIODevice::WriteOnly);
             out << QString("createNewNote");
-            qDebug() << "Sending request to create a new note";
             socket.write(block);
             socket.flush();
             socket.waitForBytesWritten();
             socket.disconnectFromServer();
-            qDebug() << "Request sent successfully";
-        } else {
-            qDebug() << "Failed to connect to server:" << socket.errorString();
         }
-        return 0; // Exit if another instance is running
+        return 0;
     }
 
-    // Create shared memory to ensure single instance
     if (!sharedMemory.create(1)) {
-        qDebug() << "Failed to create shared memory:" << sharedMemory.errorString();
         return 1;
     }
 
@@ -41,14 +31,10 @@ int main(int argc, char *argv[]) {
     app.setStyle("fusion");
     app.setQuitOnLastWindowClosed(false);
 
-    Qwote qwote;
-
-    try {
-        QwoteServer server(&qwote);
-    } catch (const std::runtime_error &e) {
-        qDebug() << "Failed to start QwoteServer:" << e.what();
+    QwoteServer qwoteServer;
+    if (!qwoteServer.startServer(SERVER_NAME)) {
         return 1;
     }
 
-    return app.exec(); // Start the application event loop
+    return app.exec();
 }
